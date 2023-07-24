39204a8eb9a5ffa351c8c67d3e2a1b9cca042f80
abbas <dilshamol.abbas@philips.com>
2023-07-24 15:14:20 +0530

    TestCode

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,33 @@
-#include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+#include "stats.h"
+#include <vector>
+#include <algorithm>
+
+
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& data ) 
+{
     //Implement statistics here
+	Statistics::Stats stats;
+		if (data.empty()) 
+		{
+			stats.average = std::nanf("");
+			stats.maximum = std::nanf("");
+			stats.minimum = std::nanf("");
+		}
+		else
+		{
+			// Calculate the average
+			float sum = 0.0f;
+			for (float value : data) 
+			{
+				sum += value;
+			}
+			stats.average = sum / static_cast<float>(data.size());
+
+			// Find the maximum and minimum values
+			stats.maximum = *std::max_element(data.begin(), data.end());
+			stats.minimum = *std::min_element(data.begin(), data.end());
+		}
+		return stats;
 }
+
--- stats.h
+++ stats.h
@@ -2,6 +2,17 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+	struct Stats
+	{
+		float average;
+		float maximum;
+		float minimum;
+
+		Stats() :average(0.0), maximum(INT_MIN), minimum(INT_MAX)
+		{
+
+		}
+		
+	};
+    Stats ComputeStatistics(const std::vector<float>& data );
 }
